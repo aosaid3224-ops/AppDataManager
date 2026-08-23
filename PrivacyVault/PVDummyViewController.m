5344794506d7b2d4d21c5e78b21b04ff04e812e3
aosaid3224-ops <aosaid3224@gmail.com>
2026-08-24 02:27:20 +0300

    PrivacyVault V0.1: PVDummyViewController.m

---
+++ PrivacyVault/PVDummyViewController.m
@@ -0,0 +1,33 @@
+#import "PVDummyViewController.h"
+
+@implementation PVDummyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"Vault";
+
+    if (@available(iOS 13.0, *)) {
+        self.view.backgroundColor = [UIColor systemBackgroundColor];
+    } else {
+        self.view.backgroundColor = [UIColor whiteColor];
+    }
+
+    UILabel *label = [[UILabel alloc] init];
+    label.text = @"لا توجد بيانات لعرضها هنا";
+    label.textAlignment = NSTextAlignmentCenter;
+    if (@available(iOS 13.0, *)) {
+        label.textColor = [UIColor labelColor];
+    } else {
+        label.textColor = [UIColor blackColor];
+    }
+    label.font = [UIFont systemFontOfSize:18];
+    label.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:label];
+
+    [NSLayoutConstraint activateConstraints:@[
+        [label.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+        [label.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor]
+    ]];
+}
+
+@end
