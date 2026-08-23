8f5b87ea0a0375f1376cb27f0e4ee505faf230c9
aosaid3224-ops <aosaid3224@gmail.com>
2026-08-24 02:27:17 +0300

    PrivacyVault V0.1: PVPasswordViewController.m

---
+++ PrivacyVault/PVPasswordViewController.m
@@ -0,0 +1,88 @@
+#import "PVPasswordViewController.h"
+#import "PVDummyViewController.h"
+
+@interface PVPasswordViewController ()
+@property (nonatomic, strong) UITextField *passwordField;
+@property (nonatomic, strong) UILabel *statusLabel;
+@end
+
+@implementation PVPasswordViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"Authentication";
+
+    if (@available(iOS 13.0, *)) {
+        self.view.backgroundColor = [UIColor systemBackgroundColor];
+    } else {
+        self.view.backgroundColor = [UIColor whiteColor];
+    }
+
+    UILabel *titleLabel = [[UILabel alloc] init];
+    titleLabel.text = @"Enter Password";
+    titleLabel.font = [UIFont boldSystemFontOfSize:20];
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:titleLabel];
+
+    self.passwordField = [[UITextField alloc] init];
+    self.passwordField.secureTextEntry = YES;
+    self.passwordField.borderStyle = UITextBorderStyleRoundedRect;
+    self.passwordField.placeholder = @"Password";
+    self.passwordField.textAlignment = NSTextAlignmentCenter;
+    self.passwordField.returnKeyType = UIReturnKeyGo;
+    self.passwordField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    self.passwordField.autocorrectionType = UITextAutocorrectionTypeNo;
+    self.passwordField.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.passwordField];
+
+    UIButton *submitBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [submitBtn setTitle:@"Unlock" forState:UIControlStateNormal];
+    submitBtn.titleLabel.font = [UIFont boldSystemFontOfSize:17];
+    [submitBtn addTarget:self action:@selector(submitTapped:) forControlEvents:UIControlEventTouchUpInside];
+    submitBtn.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:submitBtn];
+
+    self.statusLabel = [[UILabel alloc] init];
+    self.statusLabel.text = @"";
+    self.statusLabel.textAlignment = NSTextAlignmentCenter;
+    self.statusLabel.textColor = [UIColor systemRedColor];
+    self.statusLabel.font = [UIFont systemFontOfSize:14];
+    self.statusLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.statusLabel];
+
+    [NSLayoutConstraint activateConstraints:@[
+        [titleLabel.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+        [titleLabel.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:60],
+
+        [self.passwordField.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+        [self.passwordField.topAnchor constraintEqualToAnchor:titleLabel.bottomAnchor constant:30],
+        [self.passwordField.widthAnchor constraintEqualToConstant:220],
+        [self.passwordField.heightAnchor constraintEqualToConstant:44],
+
+        [submitBtn.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+        [submitBtn.topAnchor constraintEqualToAnchor:self.passwordField.bottomAnchor constant:20],
+
+        [self.statusLabel.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+        [self.statusLabel.topAnchor constraintEqualToAnchor:submitBtn.bottomAnchor constant:16]
+    ]];
+
+    [self.passwordField becomeFirstResponder];
+}
+
+- (void)submitTapped:(UIButton *)sender {
+    NSString *input = self.passwordField.text;
+    NSString *correct = @"1234";
+
+    if ([input isEqualToString:correct]) {
+        self.statusLabel.text = @"";
+        PVDummyViewController *vc = [[PVDummyViewController alloc] init];
+        [self.navigationController pushViewController:vc animated:YES];
+    } else {
+        self.statusLabel.text = @"Incorrect password";
+        self.passwordField.text = @"";
+        [self.passwordField becomeFirstResponder];
+    }
+}
+
+@end
