7b5d0ad8dcc1f62e5995840d1e760ae406df2b08
aosaid3224-ops <3.14477446e+08+aosaid3224-ops@users.noreply.github.com>
2026-08-24 04:30:49 +0000

    Promote media cards and actions to main vault V0.2.9

--- PrivacyVault/PVMediaVaultBrowserViewController.m
+++ PrivacyVault/PVMediaVaultBrowserViewController.m
@@ -1,7 +1,8 @@
 #import "PVMediaVaultBrowserViewController.h"
 #import <QuickLook/QuickLook.h>
+#import <Photos/Photos.h>
 
-@interface PVMediaVaultBrowserViewController () <UICollectionViewDataSource, UICollectionViewDelegate, QLPreviewControllerDataSource>
+@interface PVMediaVaultBrowserViewController () <UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, QLPreviewControllerDataSource>
 @property (nonatomic, strong) UICollectionView *collectionView;
 @property (nonatomic, strong) UILabel *emptyLabel;
 @property (nonatomic, strong) QLPreviewController *previewController;
@@ -18,9 +19,9 @@
     else self.view.backgroundColor = [UIColor whiteColor];
 
     UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
-    layout.minimumInteritemSpacing = 8.0;
-    layout.minimumLineSpacing = 12.0;
-    layout.sectionInset = UIEdgeInsetsMake(16, 12, 24, 12);
+    layout.minimumInteritemSpacing = 10.0;
+    layout.minimumLineSpacing = 14.0;
+    layout.sectionInset = UIEdgeInsetsMake(18, 14, 28, 14);
     self.collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:layout];
     self.collectionView.translatesAutoresizingMaskIntoConstraints = NO;
     self.collectionView.backgroundColor = UIColor.clearColor;
@@ -46,13 +47,17 @@
         [self.emptyLabel.leadingAnchor constraintGreaterThanOrEqualToAnchor:self.view.leadingAnchor constant:24],
         [self.emptyLabel.trailingAnchor constraintLessThanOrEqualToAnchor:self.view.trailingAnchor constant:-24]
     ]];
-    self.emptyLabel.hidden = self.items.count > 0;
+    [self refreshEmptyState];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    self.emptyLabel.hidden = self.items.count > 0;
+    [self refreshEmptyState];
     [self.collectionView reloadData];
+}
+
+- (void)refreshEmptyState {
+    self.emptyLabel.hidden = self.items.count > 0;
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
@@ -67,7 +72,7 @@
     UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"PVVaultGridCell" forIndexPath:indexPath];
     for (UIView *subview in [cell.contentView.subviews copy]) [subview removeFromSuperview];
     cell.backgroundColor = [UIColor secondarySystemGroupedBackgroundColor];
-    cell.layer.cornerRadius = 14.0;
+    cell.layer.cornerRadius = 16.0;
     cell.clipsToBounds = YES;
 
     PVMediaVaultItem *item = self.items[indexPath.item];
@@ -84,16 +89,6 @@
     imageView.tintColor = [UIColor systemBlueColor];
     [cell.contentView addSubview:imageView];
 
-    UIButton *restoreButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    restoreButton.translatesAutoresizingMaskIntoConstraints = NO;
-    [restoreButton setTitle:@"استرداد" forState:UIControlStateNormal];
-    restoreButton.titleLabel.font = [UIFont boldSystemFontOfSize:12];
-    restoreButton.backgroundColor = [[UIColor systemBackgroundColor] colorWithAlphaComponent:0.9];
-    restoreButton.layer.cornerRadius = 8.0;
-    restoreButton.tag = indexPath.item;
-    [restoreButton addTarget:self action:@selector(restoreTapped:) forControlEvents:UIControlEventTouchUpInside];
-    [cell.contentView addSubview:restoreButton];
-
     UILabel *nameLabel = [[UILabel alloc] init];
     nameLabel.translatesAutoresizingMaskIntoConstraints = NO;
     nameLabel.text = item.filename;
@@ -103,18 +98,25 @@
     nameLabel.lineBreakMode = NSLineBreakByTruncatingMiddle;
     [cell.contentView addSubview:nameLabel];
 
+    UILabel *typeLabel = [[UILabel alloc] init];
+    typeLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    typeLabel.text = [item.type isEqualToString:@"video"] ? @"فيديو" : @"صورة";
+    typeLabel.textAlignment = NSTextAlignmentCenter;
+    typeLabel.font = [UIFont systemFontOfSize:10];
+    typeLabel.textColor = [UIColor secondaryLabelColor];
+    [cell.contentView addSubview:typeLabel];
+
     [NSLayoutConstraint activateConstraints:@[
         [imageView.topAnchor constraintEqualToAnchor:cell.contentView.topAnchor],
         [imageView.leadingAnchor constraintEqualToAnchor:cell.contentView.leadingAnchor],
         [imageView.trailingAnchor constraintEqualToAnchor:cell.contentView.trailingAnchor],
-        [imageView.bottomAnchor constraintEqualToAnchor:nameLabel.topAnchor constant:-4],
+        [imageView.bottomAnchor constraintEqualToAnchor:typeLabel.topAnchor constant:-2],
+        [typeLabel.leadingAnchor constraintEqualToAnchor:cell.contentView.leadingAnchor constant:4],
+        [typeLabel.trailingAnchor constraintEqualToAnchor:cell.contentView.trailingAnchor constant:-4],
+        [typeLabel.bottomAnchor constraintEqualToAnchor:nameLabel.topAnchor constant:-1],
         [nameLabel.leadingAnchor constraintEqualToAnchor:cell.contentView.leadingAnchor constant:4],
         [nameLabel.trailingAnchor constraintEqualToAnchor:cell.contentView.trailingAnchor constant:-4],
-        [nameLabel.bottomAnchor constraintEqualToAnchor:cell.contentView.bottomAnchor constant:-5],
-        [restoreButton.leadingAnchor constraintEqualToAnchor:cell.contentView.leadingAnchor constant:6],
-        [restoreButton.topAnchor constraintEqualToAnchor:cell.contentView.topAnchor constant:6],
-        [restoreButton.heightAnchor constraintEqualToConstant:28],
-        [restoreButton.widthAnchor constraintEqualToConstant:58]
+        [nameLabel.bottomAnchor constraintEqualToAnchor:cell.contentView.bottomAnchor constant:-6]
     ]];
     return cell;
 }
@@ -122,16 +124,106 @@
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)layout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
     UICollectionViewFlowLayout *flowLayout = (UICollectionViewFlowLayout *)layout;
     CGFloat availableWidth = CGRectGetWidth(collectionView.bounds) - flowLayout.sectionInset.left - flowLayout.sectionInset.right - (flowLayout.minimumInteritemSpacing * 2.0);
-    CGFloat width = floor(availableWidth / 3.0);
-    return CGSizeMake(MAX(width, 90.0), 132.0);
+    return CGSizeMake(floor(availableWidth / 3.0), 154.0);
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
     if (indexPath.item >= self.items.count) return;
-    self.previewItem = self.items[indexPath.item];
+    [self showActionsForItem:self.items[indexPath.item]];
+}
+
+- (void)showActionsForItem:(PVMediaVaultItem *)item {
+    UIAlertController *actions = [UIAlertController alertControllerWithTitle:item.filename message:@"اختر الإجراء المطلوب" preferredStyle:UIAlertControllerStyleActionSheet];
+    [actions addAction:[UIAlertAction actionWithTitle:@"فتح" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        [self openItem:item];
+    }]];
+    [actions addAction:[UIAlertAction actionWithTitle:@"مشاركة" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        [self shareItem:item];
+    }]];
+    [actions addAction:[UIAlertAction actionWithTitle:@"استرداد إلى تطبيق الصور" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        if (self.restoreHandler) self.restoreHandler(item);
+        else [self restoreItemDirectly:item];
+    }]];
+    [actions addAction:[UIAlertAction actionWithTitle:@"إلغاء" style:UIAlertActionStyleCancel handler:nil]];
+    if (UIDevice.currentDevice.userInterfaceIdiom == UIUserInterfaceIdiomPad) {
+        actions.popoverPresentationController.sourceView = self.view;
+        actions.popoverPresentationController.sourceRect = CGRectMake(CGRectGetMidX(self.view.bounds), CGRectGetMidY(self.view.bounds), 1, 1);
+    }
+    [self presentViewController:actions animated:YES completion:nil];
+}
+
+- (void)openItem:(PVMediaVaultItem *)item {
+    self.previewItem = item;
     self.previewController = [[QLPreviewController alloc] init];
     self.previewController.dataSource = self;
     [self presentViewController:self.previewController animated:YES completion:nil];
+}
+
+- (void)shareItem:(PVMediaVaultItem *)item {
+    NSURL *url = [[PVMediaVaultStore sharedStore] urlForItem:item];
+    if (![[NSFileManager defaultManager] fileExistsAtPath:url.path]) {
+        [self showMessage:@"تعذر العثور على ملف الوسائط داخل PrivacyVault."];
+        return;
+    }
+    UIActivityViewController *shareController = [[UIActivityViewController alloc] initWithActivityItems:@[url] applicationActivities:nil];
+    if (UIDevice.currentDevice.userInterfaceIdiom == UIUserInterfaceIdiomPad) {
+        shareController.popoverPresentationController.sourceView = self.view;
+        shareController.popoverPresentationController.sourceRect = CGRectMake(CGRectGetMidX(self.view.bounds), CGRectGetMidY(self.view.bounds), 1, 1);
+    }
+    [self presentViewController:shareController animated:YES completion:nil];
+}
+
+- (void)restoreItemDirectly:(PVMediaVaultItem *)item {
+    NSError *verificationError = nil;
+    if (![[PVMediaVaultStore sharedStore] verifyItem:item error:&verificationError]) {
+        [self showMessage:verificationError.localizedDescription ?: @"نسخة الوسائط غير مكتملة؛ لم تتم إزالتها."];
+        return;
+    }
+    PHAuthorizationStatus authorizationStatus;
+    if (@available(iOS 14.0, *)) authorizationStatus = [PHPhotoLibrary authorizationStatusForAccessLevel:PHAccessLevelAddOnly];
+    else authorizationStatus = [PHPhotoLibrary authorizationStatus];
+    if (authorizationStatus != PHAuthorizationStatusAuthorized && authorizationStatus != PHAuthorizationStatusLimited) {
+        [self showMessage:@"يلزم السماح بإضافة الوسائط إلى تطبيق الصور قبل الاسترداد."];
+        return;
+    }
+    NSURL *url = [[PVMediaVaultStore sharedStore] urlForItem:item];
+    __block NSString *createdIdentifier = nil;
+    [PHPhotoLibrary.sharedPhotoLibrary performChanges:^{
+        PHAssetCreationRequest *request = [PHAssetCreationRequest creationRequestForAsset];
+        PHAssetResourceCreationOptions *options = [[PHAssetResourceCreationOptions alloc] init];
+        PHAssetResourceType resourceType = [item.type isEqualToString:@"video"] ? PHAssetResourceTypeVideo : PHAssetResourceTypePhoto;
+        [request addResourceWithType:resourceType fileURL:url options:options];
+        createdIdentifier = request.placeholderForCreatedAsset.localIdentifier;
+    } completionHandler:^(BOOL success, NSError *error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (!success || createdIdentifier.length == 0) {
+                [self showMessage:error.localizedDescription ?: @"فشل الاسترداد؛ بقيت نسخة Vault دون تغيير."];
+                return;
+            }
+            PHFetchResult<PHAsset *> *createdAssets = [PHAsset fetchAssetsWithLocalIdentifiers:@[createdIdentifier] options:nil];
+            if (createdAssets.count == 0) {
+                [self showMessage:@"تعذر التحقق من العنصر المسترد؛ بقيت نسخة Vault دون تغيير."];
+                return;
+            }
+            NSError *removeError = nil;
+            if (![[PVMediaVaultStore sharedStore] removeItem:item error:&removeError]) {
+                [self showMessage:@"تم الاسترداد، لكن تعذر إزالة نسخة Vault؛ بقيت النسخة للحماية."];
+                return;
+            }
+            NSMutableArray *updatedItems = [self.items mutableCopy];
+            [updatedItems removeObject:item];
+            self.items = [updatedItems copy];
+            [self refreshEmptyState];
+            [self.collectionView reloadData];
+            [self showMessage:@"تم استرداد العنصر وإزالة نسخة Vault."];
+        });
+    }];
+}
+
+- (void)showMessage:(NSString *)message {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"PrivacyVault" message:message preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"حسنًا" style:UIAlertActionStyleDefault handler:nil]];
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 - (NSInteger)numberOfPreviewItemsInPreviewController:(QLPreviewController *)controller {
@@ -139,16 +231,7 @@
 }
 
 - (id<QLPreviewItem>)previewController:(QLPreviewController *)controller previewItemAtIndex:(NSInteger)index {
-    return [self urlForPreviewItem:self.previewItem];
-}
-
-- (NSURL *)urlForPreviewItem:(PVMediaVaultItem *)item {
-    return [[PVMediaVaultStore sharedStore] urlForItem:item];
-}
-
-- (void)restoreTapped:(UIButton *)sender {
-    if (sender.tag >= self.items.count) return;
-    if (self.restoreHandler) self.restoreHandler(self.items[sender.tag]);
+    return [[PVMediaVaultStore sharedStore] urlForItem:self.previewItem];
 }
 
 @end
--- PrivacyVault/PVVaultViewController.m
+++ PrivacyVault/PVVaultViewController.m
@@ -1,58 +1,161 @@
 #import "PVVaultViewController.h"
 #import "PVSettingsViewController.h"
 #import "PVMediaLibraryViewController.h"
+#import "PVMediaVaultBrowserViewController.h"
+#import "PVMediaVaultStore.h"
+
+@interface PVVaultViewController ()
+@property (nonatomic, strong) UIButton *imagesCard;
+@property (nonatomic, strong) UIButton *videosCard;
+@property (nonatomic, strong) UILabel *subtitleLabel;
+@property (nonatomic, strong) UIButton *importButton;
+@end
 
 @implementation PVVaultViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"المستودع";
-
-    if (@available(iOS 13.0, *)) {
-        self.view.backgroundColor = [UIColor systemBackgroundColor];
-    } else {
-        self.view.backgroundColor = [UIColor whiteColor];
-    }
-
-    UILabel *label = [[UILabel alloc] init];
-    label.text = @"واجهة PrivacyVault";
-    label.textAlignment = NSTextAlignmentCenter;
-    if (@available(iOS 13.0, *)) {
-        label.textColor = [UIColor labelColor];
-    } else {
-        label.textColor = [UIColor blackColor];
-    }
-    label.font = [UIFont systemFontOfSize:20];
-    label.translatesAutoresizingMaskIntoConstraints = NO;
-    [self.view addSubview:label];
-
-    UIButton *mediaButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    [mediaButton setTitle:@"إخفاء الصور والفيديوهات" forState:UIControlStateNormal];
-    mediaButton.titleLabel.font = [UIFont boldSystemFontOfSize:17];
-    [mediaButton addTarget:self action:@selector(mediaLibraryTapped:) forControlEvents:UIControlEventTouchUpInside];
-    mediaButton.translatesAutoresizingMaskIntoConstraints = NO;
-    [self.view addSubview:mediaButton];
+    self.view.semanticContentAttribute = UISemanticContentAttributeForceRightToLeft;
+    if (@available(iOS 13.0, *)) self.view.backgroundColor = [UIColor systemBackgroundColor];
+    else self.view.backgroundColor = [UIColor whiteColor];
 
     UIButton *settingsButton = [UIButton buttonWithType:UIButtonTypeSystem];
     settingsButton.frame = CGRectMake(0, 0, 40, 40);
     settingsButton.accessibilityLabel = @"الإعدادات";
-    if (@available(iOS 13.0, *)) {
-        [settingsButton setImage:[UIImage systemImageNamed:@"gearshape.fill"] forState:UIControlStateNormal];
-    } else {
-        [settingsButton setTitle:@"⚙" forState:UIControlStateNormal];
-    }
+    if (@available(iOS 13.0, *)) [settingsButton setImage:[UIImage systemImageNamed:@"gearshape.fill"] forState:UIControlStateNormal];
+    else [settingsButton setTitle:@"⚙" forState:UIControlStateNormal];
     [settingsButton addTarget:self action:@selector(settingsTapped:) forControlEvents:UIControlEventTouchUpInside];
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:settingsButton];
 
+    UILabel *titleLabel = [[UILabel alloc] init];
+    titleLabel.text = @"PrivacyVault";
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    titleLabel.font = [UIFont systemFontOfSize:28 weight:UIFontWeightBold];
+    titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:titleLabel];
+
+    self.subtitleLabel = [[UILabel alloc] init];
+    self.subtitleLabel.text = @"مكتبتك الخاصة، مرتبة وآمنة";
+    self.subtitleLabel.textAlignment = NSTextAlignmentCenter;
+    self.subtitleLabel.textColor = [UIColor secondaryLabelColor];
+    self.subtitleLabel.font = [UIFont systemFontOfSize:15];
+    self.subtitleLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.subtitleLabel];
+
+    self.imagesCard = [self makeMediaCardWithIcon:@"photo.on.rectangle.angled" title:@"الصور" tag:1];
+    self.videosCard = [self makeMediaCardWithIcon:@"video.fill" title:@"الفيديوهات" tag:2];
+    [self.view addSubview:self.imagesCard];
+    [self.view addSubview:self.videosCard];
+
+    self.importButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.importButton setTitle:@"إضافة من تطبيق الصور" forState:UIControlStateNormal];
+    self.importButton.titleLabel.font = [UIFont boldSystemFontOfSize:16];
+    self.importButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.importButton addTarget:self action:@selector(importTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.importButton];
+
     [NSLayoutConstraint activateConstraints:@[
-        [label.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
-        [label.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor constant:-64],
-        [mediaButton.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
-        [mediaButton.topAnchor constraintEqualToAnchor:label.bottomAnchor constant:20]
+        [titleLabel.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:82],
+        [titleLabel.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:20],
+        [titleLabel.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-20],
+        [self.subtitleLabel.topAnchor constraintEqualToAnchor:titleLabel.bottomAnchor constant:8],
+        [self.subtitleLabel.leadingAnchor constraintEqualToAnchor:titleLabel.leadingAnchor],
+        [self.subtitleLabel.trailingAnchor constraintEqualToAnchor:titleLabel.trailingAnchor],
+        [self.imagesCard.topAnchor constraintEqualToAnchor:self.subtitleLabel.bottomAnchor constant:34],
+        [self.imagesCard.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:20],
+        [self.imagesCard.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:0.5 constant:-30],
+        [self.imagesCard.heightAnchor constraintEqualToConstant:190],
+        [self.videosCard.topAnchor constraintEqualToAnchor:self.imagesCard.topAnchor],
+        [self.videosCard.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-20],
+        [self.videosCard.leadingAnchor constraintEqualToAnchor:self.imagesCard.trailingAnchor constant:20],
+        [self.videosCard.heightAnchor constraintEqualToAnchor:self.imagesCard.heightAnchor],
+        [self.importButton.topAnchor constraintEqualToAnchor:self.imagesCard.bottomAnchor constant:24],
+        [self.importButton.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor]
     ]];
 }
 
-- (void)mediaLibraryTapped:(UIButton *)sender {
+- (UIButton *)makeMediaCardWithIcon:(NSString *)iconName title:(NSString *)title tag:(NSInteger)tag {
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    button.translatesAutoresizingMaskIntoConstraints = NO;
+    button.tag = tag;
+    button.accessibilityLabel = title;
+    button.layer.cornerRadius = 22.0;
+    button.clipsToBounds = YES;
+    if (@available(iOS 13.0, *)) {
+        button.backgroundColor = [UIColor secondarySystemGroupedBackgroundColor];
+        button.tintColor = [UIColor systemBlueColor];
+    } else {
+        button.backgroundColor = [UIColor colorWithWhite:0.94 alpha:1.0];
+    }
+    UIImageView *iconView = [[UIImageView alloc] init];
+    iconView.translatesAutoresizingMaskIntoConstraints = NO;
+    iconView.contentMode = UIViewContentModeScaleAspectFit;
+    if (@available(iOS 13.0, *)) iconView.image = [UIImage systemImageNamed:iconName];
+    [button addSubview:iconView];
+
+    UILabel *titleLabel = [[UILabel alloc] init];
+    titleLabel.text = title;
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    titleLabel.font = [UIFont boldSystemFontOfSize:19];
+    titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [button addSubview:titleLabel];
+
+    UILabel *countLabel = [[UILabel alloc] init];
+    countLabel.tag = 100 + tag;
+    countLabel.textAlignment = NSTextAlignmentCenter;
+    countLabel.textColor = [UIColor secondaryLabelColor];
+    countLabel.font = [UIFont systemFontOfSize:13];
+    countLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [button addSubview:countLabel];
+
+    [NSLayoutConstraint activateConstraints:@[
+        [iconView.topAnchor constraintEqualToAnchor:button.topAnchor constant:30],
+        [iconView.centerXAnchor constraintEqualToAnchor:button.centerXAnchor],
+        [iconView.widthAnchor constraintEqualToConstant:64],
+        [iconView.heightAnchor constraintEqualToConstant:64],
+        [titleLabel.topAnchor constraintEqualToAnchor:iconView.bottomAnchor constant:14],
+        [titleLabel.leadingAnchor constraintEqualToAnchor:button.leadingAnchor constant:8],
+        [titleLabel.trailingAnchor constraintEqualToAnchor:button.trailingAnchor constant:-8],
+        [countLabel.topAnchor constraintEqualToAnchor:titleLabel.bottomAnchor constant:7],
+        [countLabel.leadingAnchor constraintEqualToAnchor:titleLabel.leadingAnchor],
+        [countLabel.trailingAnchor constraintEqualToAnchor:titleLabel.trailingAnchor]
+    ]];
+    [button addTarget:self action:@selector(mediaCardTapped:) forControlEvents:UIControlEventTouchUpInside];
+    return button;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self updateCardCounts];
+}
+
+- (void)updateCardCounts {
+    NSArray<PVMediaVaultItem *> *items = [PVMediaVaultStore sharedStore].items;
+    NSUInteger images = 0;
+    NSUInteger videos = 0;
+    for (PVMediaVaultItem *item in items) {
+        if ([item.type isEqualToString:@"video"]) videos++;
+        else images++;
+    }
+    UILabel *imageCount = [self.imagesCard viewWithTag:101];
+    UILabel *videoCount = [self.videosCard viewWithTag:102];
+    imageCount.text = [NSString stringWithFormat:@"%lu عنصر مخفي", (unsigned long)images];
+    videoCount.text = [NSString stringWithFormat:@"%lu عنصر مخفي", (unsigned long)videos];
+}
+
+- (void)mediaCardTapped:(UIButton *)sender {
+    NSArray<PVMediaVaultItem *> *allItems = [PVMediaVaultStore sharedStore].items;
+    NSString *type = sender.tag == 2 ? @"video" : @"image";
+    NSMutableArray<PVMediaVaultItem *> *filteredItems = [NSMutableArray array];
+    for (PVMediaVaultItem *item in allItems) if ([item.type isEqualToString:type]) [filteredItems addObject:item];
+    PVMediaVaultBrowserViewController *browser = [[PVMediaVaultBrowserViewController alloc] init];
+    browser.items = [filteredItems copy];
+    browser.sectionTitle = sender.tag == 2 ? @"الفيديوهات المخفية" : @"الصور المخفية";
+    [self.navigationController pushViewController:browser animated:YES];
+}
+
+- (void)importTapped:(UIButton *)sender {
     PVMediaLibraryViewController *controller = [[PVMediaLibraryViewController alloc] init];
     [self.navigationController pushViewController:controller animated:YES];
 }
