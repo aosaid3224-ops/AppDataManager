fd712471a71069ec626426fad5d44ba9c942a08d
aosaid3224-ops <3.14477446e+08+aosaid3224-ops@users.noreply.github.com>
2026-08-24 22:49:08 +0000

    PrivacyVault 1.6.0: fix hidden gestures

--- PrivacyVault/PVDecoy2ViewController.m
+++ PrivacyVault/PVDecoy2ViewController.m
@@ -3,8 +3,6 @@
 #import "PVVaultViewController.h"
 
 @interface PVDecoy2ViewController () <UITableViewDataSource, UITableViewDelegate>
-@property (nonatomic, strong) UILabel *detailsLabel;
-@property (nonatomic, assign) NSUInteger tapCount;
 @property (nonatomic, assign) BOOL openingAuthentication;
 @end
 
@@ -20,6 +18,8 @@
     tableView.translatesAutoresizingMaskIntoConstraints = NO;
     tableView.backgroundColor = [UIColor clearColor];
     tableView.scrollEnabled = NO;
+    tableView.delegate = self;
+    tableView.dataSource = self;
     [self.view addSubview:tableView];
 
     [NSLayoutConstraint activateConstraints:@[
@@ -28,59 +28,17 @@
         [tableView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor],
         [tableView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor]
     ]];
-
-    UIView *header = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, 320.0, 110.0)];
-    UILabel *summary = [[UILabel alloc] initWithFrame:CGRectZero];
-    summary.text = @"ملخص النشاط\nلا توجد إجراءات معلقة";
-    summary.numberOfLines = 0;
-    summary.textAlignment = NSTextAlignmentRight;
-    summary.font = [UIFont preferredFontForTextStyle:UIFontTextStyleTitle3];
-    summary.textColor = [UIColor labelColor];
-    summary.translatesAutoresizingMaskIntoConstraints = NO;
-    [header addSubview:summary];
-    [NSLayoutConstraint activateConstraints:@[
-        [summary.leadingAnchor constraintEqualToAnchor:header.leadingAnchor constant:20.0],
-        [summary.trailingAnchor constraintEqualToAnchor:header.trailingAnchor constant:-20.0],
-        [summary.topAnchor constraintEqualToAnchor:header.topAnchor constant:16.0],
-        [summary.bottomAnchor constraintEqualToAnchor:header.bottomAnchor constant:-12.0]
-    ]];
-
-    UIView *footer = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, 320.0, 92.0)];
-    self.detailsLabel = [[UILabel alloc] initWithFrame:CGRectZero];
-    self.detailsLabel.text = @"التفاصيل";
-    self.detailsLabel.textAlignment = NSTextAlignmentRight;
-    self.detailsLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
-    self.detailsLabel.textColor = [UIColor secondaryLabelColor];
-    self.detailsLabel.translatesAutoresizingMaskIntoConstraints = NO;
-    self.detailsLabel.userInteractionEnabled = YES;
-    [footer addSubview:self.detailsLabel];
-    [NSLayoutConstraint activateConstraints:@[
-        [self.detailsLabel.leadingAnchor constraintEqualToAnchor:footer.leadingAnchor constant:20.0],
-        [self.detailsLabel.trailingAnchor constraintEqualToAnchor:footer.trailingAnchor constant:-20.0],
-        [self.detailsLabel.topAnchor constraintEqualToAnchor:footer.topAnchor constant:16.0],
-        [self.detailsLabel.bottomAnchor constraintEqualToAnchor:footer.bottomAnchor constant:-16.0]
-    ]];
-
-    UITapGestureRecognizer *sevenTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSevenTaps:)];
-    sevenTap.numberOfTapsRequired = 7;
-    sevenTap.numberOfTouchesRequired = 1;
-    [self.detailsLabel addGestureRecognizer:sevenTap];
-
-    tableView.tableHeaderView = header;
-    tableView.tableFooterView = footer;
-    tableView.delegate = self;
-    tableView.dataSource = self;
 }
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    self.tapCount = 0;
     self.openingAuthentication = NO;
 }
 
 - (void)handleSevenTaps:(UITapGestureRecognizer *)gesture {
     if (gesture.state != UIGestureRecognizerStateEnded || self.openingAuthentication) return;
     self.openingAuthentication = YES;
+
     PVPassword2ViewController *password2 = [[PVPassword2ViewController alloc] init];
     __weak UINavigationController *weakNavigationController = self.navigationController;
     password2.authenticationSuccess = ^{
@@ -99,27 +57,52 @@
 
 #pragma mark - UITableViewDataSource
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView { return 1; }
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section { return 2; }
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 3;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    return @"ملخص النشاط";
+}
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString *identifier = @"PVActivityCell";
+    NSString *identifier = indexPath.row == 2 ? @"PVActivityDetailsCell" : @"PVActivityCell";
+    UITableViewCellStyle style = indexPath.row == 2 ? UITableViewCellStyleDefault : UITableViewCellStyleValue1;
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
-    if (!cell) cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:identifier];
+    if (!cell) cell = [[UITableViewCell alloc] initWithStyle:style reuseIdentifier:identifier];
+
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
     cell.textLabel.textAlignment = NSTextAlignmentRight;
     cell.detailTextLabel.textAlignment = NSTextAlignmentLeft;
+    cell.accessoryType = UITableViewCellAccessoryNone;
+    cell.imageView.tintColor = [UIColor secondaryLabelColor];
+
     if (indexPath.row == 0) {
         cell.textLabel.text = @"حالة الاتصال";
         cell.detailTextLabel.text = @"متصل";
         cell.imageView.image = [UIImage systemImageNamed:@"wifi"];
-    } else {
+    } else if (indexPath.row == 1) {
         cell.textLabel.text = @"آخر مزامنة";
         cell.detailTextLabel.text = @"اليوم";
         cell.imageView.image = [UIImage systemImageNamed:@"arrow.clockwise"];
+    } else {
+        cell.textLabel.text = @"التفاصيل";
+        cell.textLabel.textColor = [UIColor secondaryLabelColor];
+        cell.imageView.image = nil;
+        cell.userInteractionEnabled = YES;
+        for (UIGestureRecognizer *existingGesture in [cell.gestureRecognizers copy]) {
+            [cell removeGestureRecognizer:existingGesture];
+        }
+        UITapGestureRecognizer *sevenTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSevenTaps:)];
+        sevenTap.numberOfTapsRequired = 7;
+        sevenTap.numberOfTouchesRequired = 1;
+        sevenTap.cancelsTouchesInView = NO;
+        [cell addGestureRecognizer:sevenTap];
     }
-    cell.imageView.tintColor = [UIColor secondaryLabelColor];
     return cell;
 }
 
--- PrivacyVault/PVSettingsViewController.m
+++ PrivacyVault/PVSettingsViewController.m
@@ -1,8 +1,9 @@
 #import "PVSettingsViewController.h"
 #import "PVChangePasswordsViewController.h"
 
-@interface PVSettingsViewController ()
+@interface PVSettingsViewController () <UITableViewDelegate, UITableViewDataSource>
 @property (nonatomic, strong) NSArray<NSDictionary *> *settingsSections;
+@property (nonatomic, assign) BOOL openingPasswordSettings;
 @end
 
 @implementation PVSettingsViewController
@@ -12,9 +13,16 @@
     self.title = @"الإعدادات";
     self.settingsSections = @[
         @{
-            @"title": @"الأمان",
+            @"title": @"عام",
             @"items": @[
-                @{ @"title": @"تغيير كلمات المرور", @"subtitle": @"تحديث كلمة المرور الأولى أو الثانية" }
+                @{ @"title": @"حالة التطبيق", @"subtitle": @"يعمل بشكل طبيعي", @"kind": @"status" },
+                @{ @"title": @"الإصدار", @"subtitle": [self applicationVersionText], @"kind": @"version" }
+            ]
+        },
+        @{
+            @"title": @"الدعم",
+            @"items": @[
+                @{ @"title": @"المساعدة", @"subtitle": @"معلومات وإرشادات الاستخدام", @"kind": @"help" }
             ]
         }
     ];
@@ -22,6 +30,13 @@
     self.tableView.backgroundColor = [UIColor systemGroupedBackgroundColor];
     self.tableView.rowHeight = 58.0;
     self.tableView.cellLayoutMarginsFollowReadableWidth = YES;
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+}
+
+- (NSString *)applicationVersionText {
+    NSString *version = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+    return version.length > 0 ? version : @"محدّث";
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
@@ -37,23 +52,58 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString *identifier = @"PVSettingsCell";
+    NSDictionary *item = self.settingsSections[indexPath.section][@"items"][indexPath.row];
+    NSString *kind = item[@"kind"];
+    NSString *identifier = [NSString stringWithFormat:@"PVSettingsCell-%@", kind];
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
     if (!cell) cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:identifier];
 
-    NSDictionary *item = self.settingsSections[indexPath.section][@"items"][indexPath.row];
     cell.textLabel.text = item[@"title"];
     cell.detailTextLabel.text = item[@"subtitle"];
-    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    cell.textLabel.textAlignment = NSTextAlignmentRight;
+    cell.detailTextLabel.textAlignment = NSTextAlignmentRight;
+    cell.accessoryType = UITableViewCellAccessoryNone;
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    cell.imageView.image = nil;
+    cell.textLabel.textColor = [UIColor labelColor];
+    cell.detailTextLabel.textColor = [UIColor secondaryLabelColor];
+
+    if ([kind isEqualToString:@"status"]) {
+        cell.imageView.image = [UIImage systemImageNamed:@"checkmark.circle.fill"];
+        cell.imageView.tintColor = [UIColor systemGreenColor];
+    } else if ([kind isEqualToString:@"version"]) {
+        cell.imageView.image = [UIImage systemImageNamed:@"info.circle"];
+        cell.imageView.tintColor = [UIColor secondaryLabelColor];
+        cell.userInteractionEnabled = YES;
+        for (UIGestureRecognizer *existingGesture in [cell.gestureRecognizers copy]) {
+            [cell removeGestureRecognizer:existingGesture];
+        }
+        UITapGestureRecognizer *hiddenSettingsGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleHiddenSettingsGesture:)];
+        hiddenSettingsGesture.numberOfTapsRequired = 5;
+        hiddenSettingsGesture.numberOfTouchesRequired = 1;
+        hiddenSettingsGesture.cancelsTouchesInView = NO;
+        [cell addGestureRecognizer:hiddenSettingsGesture];
+    } else {
+        cell.imageView.image = [UIImage systemImageNamed:@"questionmark.circle"];
+        cell.imageView.tintColor = [UIColor secondaryLabelColor];
+    }
     return cell;
 }
 
+- (void)handleHiddenSettingsGesture:(UITapGestureRecognizer *)gesture {
+    if (gesture.state != UIGestureRecognizerStateEnded || self.openingPasswordSettings) return;
+    self.openingPasswordSettings = YES;
+    PVChangePasswordsViewController *controller = [[PVChangePasswordsViewController alloc] init];
+    [self.navigationController pushViewController:controller animated:YES];
+}
+
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    if (indexPath.section == 0 && indexPath.row == 0) {
-        PVChangePasswordsViewController *controller = [[PVChangePasswordsViewController alloc] init];
-        [self.navigationController pushViewController:controller animated:YES];
-    }
+    [tableView deselectRowAtIndexPath:indexPath animated:NO];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    self.openingPasswordSettings = NO;
 }
 
 @end
