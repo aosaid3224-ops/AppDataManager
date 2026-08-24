374a95d0a87ad0ec711880b7ffeccfc332d6e19b
aosaid3224-ops <aosaid3224@gmail.com>
2026-08-24 04:32:39 +0300

    V0.1 App: PVVaultViewController.m

---
+++ PrivacyVault/PVVaultViewController.m
@@ -0,0 +1,33 @@
+#import "PVVaultViewController.h"
+
+@implementation PVVaultViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"المستودع";
+
+    if (@available(iOS 13.0, *)) {
+        self.view.backgroundColor = [UIColor systemBackgroundColor];
+    } else {
+        self.view.backgroundColor = [UIColor whiteColor];
+    }
+
+    UILabel *label = [[UILabel alloc] init];
+    label.text = @"واجهة PrivacyVault";
+    label.textAlignment = NSTextAlignmentCenter;
+    if (@available(iOS 13.0, *)) {
+        label.textColor = [UIColor labelColor];
+    } else {
+        label.textColor = [UIColor blackColor];
+    }
+    label.font = [UIFont systemFontOfSize:20];
+    label.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:label];
+
+    [NSLayoutConstraint activateConstraints:@[
+        [label.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+        [label.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor]
+    ]];
+}
+
+@end
