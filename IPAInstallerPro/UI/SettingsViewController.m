0374f7f2bf26ef41276231e769cfb9e4db0643d8
aosaid3224-ops <3.14477446e+08+aosaid3224-ops@users.noreply.github.com>
2026-08-30 19:24:14 +0000

    Make about section expandable in settings

--- IPAInstallerPro/UI/SettingsViewController.m
+++ IPAInstallerPro/UI/SettingsViewController.m
@@ -30,7 +30,7 @@
     [self setupScrollView];
     [self setupEnvironmentSection];
     [self setupCapabilitiesSection];
-    [self setupAboutSection];
+    [self setupAboutRow];
     [self refreshData];
 }
 
@@ -129,64 +129,40 @@
 
 #pragma mark - About Section
 
-- (void)setupAboutSection {
-    UILabel *header = [[UILabel alloc] init];
-    header.translatesAutoresizingMaskIntoConstraints = NO;
-    header.text = @"حول الأداة";
-    header.font = [UIFont systemFontOfSize:18 weight:UIFontWeightBold];
-    header.textColor = [UIColor whiteColor];
-    header.textAlignment = NSTextAlignmentRight;
-    [self.contentView addSubview:header];
-
-    UILabel *aboutLabel = [[UILabel alloc] init];
-    aboutLabel.translatesAutoresizingMaskIntoConstraints = NO;
-    aboutLabel.text = @"هذه الأداة متاحة حاليًا كنسخة تجريبية وليست الإصدار النهائي.\nقد تواجه بعض الأخطاء أو المشاكل أثناء الاستخدام، ونهدف من خلال هذه المرحلة إلى اختبار الأداة وتحسين استقرارها وتطوير ميزاتها.\n\nإذا واجهت أي خلل، أو لديك ملاحظة أو اقتراح لتحسين الأداة، نرجو منك مشاركة تجربتك معنا. ملاحظاتك تساعدنا على اكتشاف المشاكل ومعالجتها قبل إطلاق الإصدار النهائي.\n\nللتواصل والإبلاغ عن المشاكل:\nX: @Zainqkvd";
-    aboutLabel.font = [UIFont systemFontOfSize:14 weight:UIFontWeightRegular];
-    aboutLabel.textColor = [UIColor colorWithWhite:0.82 alpha:1.0];
-    aboutLabel.backgroundColor = [IPTheme cardColor];
-    aboutLabel.layer.borderWidth = 0.7;
-    aboutLabel.layer.borderColor = [IPTheme subtleBorderColor].CGColor;
-    aboutLabel.layer.cornerRadius = 16;
-    aboutLabel.clipsToBounds = YES;
-    aboutLabel.numberOfLines = 0;
-    aboutLabel.textAlignment = NSTextAlignmentRight;
-    aboutLabel.lineBreakMode = NSLineBreakByWordWrapping;
-    [self.contentView addSubview:aboutLabel];
-
-    UIButton *okButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    okButton.translatesAutoresizingMaskIntoConstraints = NO;
-    [okButton setTitle:@"حسناً" forState:UIControlStateNormal];
-    [okButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    okButton.titleLabel.font = [UIFont systemFontOfSize:16 weight:UIFontWeightSemibold];
-    okButton.backgroundColor = [IPTheme accentColor];
-    okButton.layer.cornerRadius = 12;
-    [okButton addTarget:self action:@selector(closeAbout:) forControlEvents:UIControlEventTouchUpInside];
-    [self.contentView addSubview:okButton];
+- (void)setupAboutRow {
+    UIButton *aboutButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    aboutButton.translatesAutoresizingMaskIntoConstraints = NO;
+    aboutButton.backgroundColor = [IPTheme cardColor];
+    aboutButton.layer.borderWidth = 0.7;
+    aboutButton.layer.borderColor = [IPTheme subtleBorderColor].CGColor;
+    aboutButton.layer.cornerRadius = 14;
+    aboutButton.clipsToBounds = YES;
+    aboutButton.contentHorizontalAlignment = UIControlContentHorizontalAlignmentRight;
+    aboutButton.contentEdgeInsets = UIEdgeInsetsMake(0, 18, 0, 18);
+    aboutButton.semanticContentAttribute = UISemanticContentAttributeForceRightToLeft;
+    [aboutButton setTitle:@"حول الأداة" forState:UIControlStateNormal];
+    [aboutButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    aboutButton.titleLabel.font = [UIFont systemFontOfSize:17 weight:UIFontWeightSemibold];
+    [aboutButton setImage:[UIImage systemImageNamed:@"chevron.left"] forState:UIControlStateNormal];
+    aboutButton.tintColor = [IPTheme mutedTextColor];
+    aboutButton.imageEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 12);
+    [aboutButton addTarget:self action:@selector(showAbout:) forControlEvents:UIControlEventTouchUpInside];
+    [self.contentView addSubview:aboutButton];
 
     [NSLayoutConstraint activateConstraints:@[
-        [header.topAnchor constraintEqualToAnchor:self.capLabel.bottomAnchor constant:24],
-        [header.leadingAnchor constraintEqualToAnchor:self.contentView.leadingAnchor constant:16],
-        [header.trailingAnchor constraintEqualToAnchor:self.contentView.trailingAnchor constant:-16],
-
-        [aboutLabel.topAnchor constraintEqualToAnchor:header.bottomAnchor constant:8],
-        [aboutLabel.leadingAnchor constraintEqualToAnchor:self.contentView.leadingAnchor constant:16],
-        [aboutLabel.trailingAnchor constraintEqualToAnchor:self.contentView.trailingAnchor constant:-16],
-        [aboutLabel.heightAnchor constraintGreaterThanOrEqualToConstant:260],
-
-        [okButton.topAnchor constraintEqualToAnchor:aboutLabel.bottomAnchor constant:16],
-        [okButton.centerXAnchor constraintEqualToAnchor:self.contentView.centerXAnchor],
-        [okButton.widthAnchor constraintEqualToConstant:120],
-        [okButton.heightAnchor constraintEqualToConstant:44],
-        [okButton.bottomAnchor constraintEqualToAnchor:self.contentView.bottomAnchor constant:-24]
+        [aboutButton.topAnchor constraintEqualToAnchor:self.capLabel.bottomAnchor constant:24],
+        [aboutButton.leadingAnchor constraintEqualToAnchor:self.contentView.leadingAnchor constant:16],
+        [aboutButton.trailingAnchor constraintEqualToAnchor:self.contentView.trailingAnchor constant:-16],
+        [aboutButton.heightAnchor constraintEqualToConstant:60],
+        [aboutButton.bottomAnchor constraintEqualToAnchor:self.contentView.bottomAnchor constant:-24]
     ]];
 }
 
-- (void)closeAbout:(UIButton *)sender {
-    if (self.navigationController.presentingViewController) {
-        [self.navigationController dismissViewControllerAnimated:YES completion:nil];
-    } else {
-        [self.navigationController popViewControllerAnimated:YES];
-    }
+- (void)showAbout:(UIButton *)sender {
+    NSString *message = @"هذه الأداة متاحة حاليًا كنسخة تجريبية وليست الإصدار النهائي.\n\nقد تواجه بعض الأخطاء أو المشاكل أثناء الاستخدام، ونهدف من خلال هذه المرحلة إلى اختبار الأداة وتحسين استقرارها وتطوير ميزاتها.\n\nإذا واجهت أي خلل، أو لديك ملاحظة أو اقتراح لتحسين الأداة، نرجو منك مشاركة تجربتك معنا. ملاحظاتك تساعدنا على اكتشاف المشاكل ومعالجتها قبل إطلاق الإصدار النهائي.\n\nللتواصل والإبلاغ عن المشاكل:\nX: @Zainqkvd";
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"حول الأداة" message:message preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"حسناً" style:UIAlertActionStyleDefault handler:nil]];
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 #pragma mark - Data
