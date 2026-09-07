1fc31ea2fa04a0a337ec3b0c2d2d27c2703b3946
aosaid3224-ops <aosaid3224@gmail.com>
2026-09-07 15:53:50 +0300

    env: Add SpiderJailbreakTypeRelaxin for RootHide/Relaxin detection

--- IPAInstallerPro/Core/RuntimeEnvironment.h
+++ IPAInstallerPro/Core/RuntimeEnvironment.h
@@ -18,6 +18,7 @@ typedef NS_ENUM(NSInteger, SpiderJailbreakType) {
     SpiderJailbreakTypeTaurine,
     SpiderJailbreakTypeOdyssey,
     SpiderJailbreakTypeXinaA15,
+    SpiderJailbreakTypeRelaxin,
     SpiderJailbreakTypeOther
 };
 
